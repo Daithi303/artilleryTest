e1d6e4a5de457463ae9d5e520562dc9e4bee8223
“David <dodwyer@kargo.com>
2022-06-22 10:38:11 +0100

    initial commit

---
+++ processor.js
@@ -0,0 +1,14 @@
+// The JS file is expected to be a standard Node.js module:
+
+module.exports = {
+    beforeRequestHandler (requestParams, context, ee, next) {
+      return next() // Must be called for the scenario to continue
+    },
+    afterResponseHandler (requestParams, response, context, ee, next) {
+        // add a length field to the response body
+        body=JSON.parse(response.body);
+    //    console.log(`response body : ${response.body}`)
+       console.log(`response body length : ${body.length}`)
+      return next() // Must be called for the scenario to continue
+    }
+  }
